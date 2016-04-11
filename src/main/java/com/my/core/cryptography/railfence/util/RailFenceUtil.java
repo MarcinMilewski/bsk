6ecd1f31062cfb87eb578cbf12695dbf513ea4c8
Marcin Milewski <marcin.milewski@mail.com>
2016-04-11 14:42:48 +0200

    upDownList generation fixed.

--- src/main/java/com/my/core/cryptography/railfence/util/RailFenceUtil.java
+++ src/main/java/com/my/core/cryptography/railfence/util/RailFenceUtil.java
@@ -3,6 +3,7 @@ package com.my.core.cryptography.railfence.util;
 import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,11 +11,18 @@ import java.util.stream.IntStream;
 public class RailFenceUtil {
     public static List<Integer> upDownList(int depth, int length) {
         List<Integer> upDownList = Lists.newArrayList();
+        if (depth <= 1) return getIdentityList(length);
         for (int i = 0; i < length; i += 2 * depth- 2) {
             upDownList.addAll(range(1, depth));
             upDownList.addAll(range(depth, 2));
         }
         return truncate(upDownList, length);
+    }
+
+    private static List<Integer> getIdentityList(int length) {
+        Integer[] integers = new Integer[length];
+        Arrays.fill(integers, 1);
+        return Arrays.asList(integers);
     }
 
     public static List<Integer> truncate(List<Integer> upDownList, int length) {
