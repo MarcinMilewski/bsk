4a3cbc40c588134eeefb384f58b24e1f303f0c98
marcin <marcin.milewski@mail.com>
2016-05-05 13:30:33 +0200

    initial permutation untested

--- src/main/java/com/my/core/cryptography/des/DesAlgorithm.java
+++ src/main/java/com/my/core/cryptography/des/DesAlgorithm.java
@@ -1,8 +1,21 @@
 package com.my.core.cryptography.des;
 
+import static com.my.core.cryptography.des.DesUtils.*;
+
 public class DesAlgorithm {
-    public  boolean[] initialBlockPermutate(boolean[] block) {
+    public  boolean[] initialPermutate(boolean[] block) {
         boolean[] result = new boolean[64];
-        return null;
+        for (int i = 0; i < 64; ++i) {
+            result[i] = block[initialPermutationLUT[i]];
+        }
+        return result;
+    }
+
+    public  boolean[] finalPermutate(boolean[] block) {
+        boolean[] result = new boolean[64];
+        for (int i = 0; i < 64; ++i) {
+            result[i] = block[finalPermutationLUT[i]];
+        }
+        return result;
     }
 }
