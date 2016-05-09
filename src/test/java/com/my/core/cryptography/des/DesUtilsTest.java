276ff597eac268a464d1a255fd14af7417863362
marcin <marcin.milewski@mail.com>
2016-05-09 14:20:21 +0200

    added sboxes

--- src/test/java/com/my/core/cryptography/des/DesUtilsTest.java
+++ src/test/java/com/my/core/cryptography/des/DesUtilsTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static com.my.core.cryptography.des.DesUtils.createBlocks;
+import static com.my.core.cryptography.des.DesUtils.sBox1;
+import static com.my.core.cryptography.generator.stream.util.BinaryUtils.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -23,5 +25,12 @@ public class DesUtilsTest {
         assertThat(blocks.length, is(2));
     }
 
+    @Test
+    public void sBox1Test() throws Exception {
+        assertThat(sBox1[0][0], is(b14));
+        assertThat(sBox1[1][1], is(b15));
+        assertThat(sBox1[2][15], is(b0));
+        assertThat(sBox1[3][15], is(b13));
+    }
 
 }
